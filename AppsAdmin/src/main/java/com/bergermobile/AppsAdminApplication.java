cd2fbfd53f91066ff79ee5cfcc5e331e238a5df1
Fabio Berger <fabio@bergermobile.com.br>
2016-01-04 12:10:49 -0200

    1st draft - Initial Spring Boot application, configured for this project

---
+++ AppsAdmin/src/main/java/com/bergermobile/AppsAdminApplication.java
@@ -0,0 +1,12 @@
+package com.bergermobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppsAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppsAdminApplication.class, args);
+	}
+}
