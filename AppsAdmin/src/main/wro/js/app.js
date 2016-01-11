58c744de19ed62d836b7e9815b963fc6f05604e2
Fabio Berger <fabio@bergermobile.com.br>
2016-01-11 15:22:52 -0200

    Downgrading to Spring Boot 1.2.8 because wro.xml was not being correctly filtered with variables, by Maven

--- AppsAdmin/src/main/wro/js/app.js
+++ AppsAdmin/src/main/wro/js/app.js
@@ -12,7 +12,7 @@ angular.module('appsadmin', [
   'bmauth.common',
   'appsadmin.navigation'
  ])
-
+ 
 .config(['$routeProvider', '$translateProvider', '$locationProvider', '$httpProvider','FacebookProvider','GooglePlusProvider',
          function($routeProvider, $translateProvider, $locationProvider, $httpProvider, FacebookProvider, GooglePlusProvider) {
 	// routes configuration
@@ -34,14 +34,14 @@ angular.module('appsadmin', [
 	//$translateProvider.useSanitizeValueStrategy('escape');
 	$translateProvider.useSanitizeValueStrategy('escapeParameters');
     $translateProvider.translations('en', bmauth_translations.en);
-	
+
 	$translateProvider.preferredLanguage('en');
 	$translateProvider.fallbackLanguage('en');
 	
 	// support natural routes
 	$locationProvider.html5Mode(true);
 }])
-
+  
 .run(['DTDefaultOptions','$translate','$rootScope','auth', function(DTDefaultOptions, $translate, $rootScope, auth) {
     //DTDefaultOptions.setLanguageSource('fragments/lang/dtlang-' + $translate.use() + '.json');
     $rootScope.authContext='bmauth/';	// for reverse proxy
